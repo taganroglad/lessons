06986688f4b6be5b6031770760e587ca855276d3
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-03 22:12:44 +0300

    just created my own code

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.WriteLine("Print any five-digit number");
+String? str = Console.ReadLine();
+if (str.Length != 5)
+{
+    System.Console.WriteLine("Incorrect data");
+}
+else if (str![0] == str![4] && str![1] == str![3])
+{
+    System.Console.WriteLine("yes, this is palindrom");
+}
+else
+{
+    System.Console.WriteLine("no, this is not palindrom");
+}
