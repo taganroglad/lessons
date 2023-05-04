825e1e52111c43fc18ce8f011a6f273d325702be
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-04 22:18:54 +0300

    just added my own code for task 21

---
+++ task_21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DistanceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("print cords for  A (x,y,z):");
+            string[] a = Console.ReadLine().Split(',');
+
+            Console.WriteLine("print cords for B (x,y,z):");
+            string[] b = Console.ReadLine().Split(',');
+
+            double x1 = Convert.ToDouble(a[0].Trim());
+            double y1 = Convert.ToDouble(a[1].Trim());
+            double z1 = Convert.ToDouble(a[2].Trim());
+
+            double x2 = Convert.ToDouble(b[0].Trim());
+            double y2 = Convert.ToDouble(b[1].Trim());
+            double z2 = Convert.ToDouble(b[2].Trim());
+
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+            Console.WriteLine($"Distance between A and B: {distance:F2}");
+        }
+    }
+}
