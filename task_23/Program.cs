e172616a8af7c9dd2034d4b22d522ce17c136f97
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-04 22:52:11 +0300

    just added my own code for task 23

---
+++ task_23/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Print a digit N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            int cube = i * i * i;
+            Console.Write($"{cube} ");
+        }
+
+        Console.WriteLine();
+    }
+}
