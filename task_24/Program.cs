f0ba10a9a79481bcb7bc0c3a078483732ac24f7a
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-20 11:11:43 +0300

    hren

--- task_24/Program.cs
+++ task_24/Program.cs
@@ -1,19 +1,11 @@
-﻿// программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
-//7-> 28
-//4-> 10
-//8-> 36
-int Readint(string text)
+﻿
+
+using System.Diagnostics.CodeAnalysis;
+
+int ReadInt(string text)
 {
     System.Console.WriteLine(text);
     return Convert.ToInt32(Console.ReadLine());
 }
-System.Console.WriteLine("print the digit");
-int number = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
 
-for (int i = 1; i <= number; i++)
-{
-    sum += i;
-
-}
-System.Console.WriteLine(sum);
+Su
