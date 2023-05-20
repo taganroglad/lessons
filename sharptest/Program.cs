4f6d236f1e68d0faa06ea928eb168f463fc062e9
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-20 10:39:07 +0300

    20.05.23

--- sharptest/Program.cs
+++ sharptest/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7-> 28
+//4-> 10
+//8-> 36
+
+System.Console.WriteLine("print the digit");
+int number  = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for  (int i = 1; i <= number; i++)
+{
+    sum += i;
+
+}
+System.Console.WriteLine(sum);
