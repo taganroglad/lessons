069213dc63c95a402c16535bc90d7fc837177f14
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-20 18:21:21 +0300

    just added 24th task

--- task_24/Program.cs
+++ task_24/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int summa = 0;
+int number = ReadInt("print the digit:   ");
+
+
+while (number != 0)
+{
+    number /= 10;
+    summa += 1;
+}
+
+System.Console.WriteLine(summa);
