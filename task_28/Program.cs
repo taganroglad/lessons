02a70d9cd1e0f3b82a0256d18843ead624c896ef
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-20 18:32:36 +0300

    just added 28 code

--- task_28/Program.cs
+++ task_28/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadInt("insert the number:   ");
+
+int factorial = 1;
+for (int i = 1;  i <= number; i++)
+{
+    factorial = factorial * i;
+}
+System.Console.WriteLine(factorial);
