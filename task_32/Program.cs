85817f50bab39c924f88ca08a2294f851285da43
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-21 11:15:52 +0300

    just added 32 slow code

---
+++ task_32/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие
+// отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return tempArray;
+}
