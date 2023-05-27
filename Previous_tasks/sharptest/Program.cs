9d7ce4b524ce813fa397286fe05b2d61035a1ced
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-27 14:52:30 +0300

    just added 10_test

--- Previous_tasks/sharptest/Program.cs
+++ Previous_tasks/sharptest/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+    
+}
+
+int number = Promt("put three digit number");
+if (number < 100 ||  number > 999)
+{
+    Console.WriteLine("this is not three digit number");
+    return;
+}
+int secondrank = number / 10 % 10;
+Console.WriteLine($"second digit is {secondrank}");
---
+++ Previous_tasks/test2/Program.cs
@@ -0,0 +1,15 @@
+﻿int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    string value = System.Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int chislo = Promt("print three-digit number");
+if (chislo > 999 || chislo <100)
+{
+    Console.WriteLine("this is not three digit");
+}
+int seconrank = chislo / 10 % 10;
+Console.WriteLine($"second rank is {seconrank}");
