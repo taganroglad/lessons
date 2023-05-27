8e7ba91e44008013e4997a3d2bf7de485ab7e331
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-27 15:27:11 +0300

    just added 10_test

---
+++ Previous_tasks/for_13/Program.cs
@@ -0,0 +1,15 @@
+﻿int counting(string message)
+{
+    Console.Write(message);
+    string value = Console.Readline();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int x = counting("pring any three-digit number");
+if (x > 999 || x < 100)
+{
+    Console.WriteLine("This is not three digit number");
+    return;
+}
+int thirdrank = x / 100 % 100;
+System.Console.WriteLine($"The third digit is {thirdrank}");
