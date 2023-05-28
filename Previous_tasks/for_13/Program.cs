a19f665ca02baf8c9285f2fd0c23e83577d694cf
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-28 11:40:13 +0300

    just added 13 code

--- Previous_tasks/for_13/Program.cs
+++ Previous_tasks/for_13/Program.cs
@@ -1,15 +1,26 @@
-﻿int counting(string message)
+﻿using System;
+
+public class Program
 {
-    Console.Write(message);
-    string value = Console.Readline();
-    int result = Convert.ToInt32(value);
-    return result;
+    public static int Counting(string message)
+    {
+        Console.WriteLine(message);
+        string value = Console.ReadLine();
+        int result = Convert.ToInt32(value);
+        return result;
+    }
+
+    public static void Main()
+    {
+        int x = Counting("Print any three-digit number");
+
+        if (x > 999 || x < 100)
+        {
+            Console.WriteLine("This is not a three-digit number");
+            return;
+        }
+
+        int thirdDigit = x % 10;
+        Console.WriteLine($"The third digit is {thirdDigit}");
+    }
 }
-int x = counting("pring any three-digit number");
-if (x > 999 || x < 100)
-{
-    Console.WriteLine("This is not three digit number");
-    return;
-}
-int thirdrank = x / 100 % 100;
-System.Console.WriteLine($"The third digit is {thirdrank}");
