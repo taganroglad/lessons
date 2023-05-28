134c8d25e599e2a76a624ac7b9cdaff8745fc6ea
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-28 11:29:49 +0300

    just added 48 code

--- Previous_tasks/task_48/Program.cs
+++ Previous_tasks/task_48/Program.cs
@@ -1,16 +1,43 @@
-﻿int ReadInt(string text)
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+//  каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+// Выведите полученный массив на экран.
+// rows = 3, cols = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+int ReadInt(string text)
 {
     System.Console.Write(text);
     return Convert.ToInt32(Console.ReadLine());
 }
-int m = ReadInt("print the number of columns  ");
-int n = ReadInt("print the number of rows  ");
 
-int [,] Macciv = new int[m, n];
-for (int i = 0; i < Macciv.GetLength(0); i++)
+int[,] FillMatrix(int m, int n)
 {
-    for (int j = 0; j < Macciv.GetLength(1); j++)
+    int[,] Macciv = new int[m, n];
+    for (int i = 0; i < Macciv.GetLength(dimension: 0); i++)
     {
-        Macciv[i, j] = i + j;
+        for (int j = 0; j < Macciv.GetLength(dimension: 1); j++)
+        {
+            Macciv[i, j] = i + j;
+        }
+
+    }
+    return Macciv;
+}
+
+void PrintMatrix(int[,] Macciv)
+{
+    for (int i = 0; i < Macciv.GetLength(dimension: 0); i++)
+    {
+        for (int j = 0; j < Macciv.GetLength(dimension: 1); j++)
+        {
+            System.Console.Write(Macciv[i, j] + " ");
+        }
+        System.Console.WriteLine();
     }
 }
+
+int[,] Matrix = FillMatrix(ReadInt("print the number of columns: "),
+                            ReadInt("print the number of rows: "));
+
+PrintMatrix(Matrix);
