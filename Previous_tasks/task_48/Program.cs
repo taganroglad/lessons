6a2a57683d5c6d1b17d5544f1e2fe207cee55099
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-28 11:23:52 +0300

    just added 48 row

--- Previous_tasks/task_48/Program.cs
+++ Previous_tasks/task_48/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = ReadInt("print the number of columns  ");
+int n = ReadInt("print the number of rows  ");
+
+int [,] Macciv = new int[m, n];
+for (int i = 0; i < Macciv.GetLength(0); i++)
+{
+    for (int j = 0; j < Macciv.GetLength(1); j++)
+    {
+        Macciv[i, j] = i + j;
+    }
+}
