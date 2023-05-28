d82c84f4e7ef9bc37753e13e61aa019cafa6baef
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-28 11:53:51 +0300

    just added 15 code

--- Previous_tasks/for_15/Program.cs
+++ Previous_tasks/for_15/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Den(string text)
+{
+    Console.WriteLine(text);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int vhod = Den("Print the number of the day");
+    if (vhod > 5) 
+    {
+    Console.WriteLine("This day is weekend");
+    }
+    else Console.WriteLine("This is weekday");
