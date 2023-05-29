fd3f96d51b6f9dc61878741afafc626033ec3d66
Alexey Agafonov <taganroglad1@gmail.com>
2023-05-29 22:19:44 +0300

    added some practice

--- Previous_tasks/for_19/Program.cs
+++ Previous_tasks/for_19/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("print any five-digit number");
+string? x = Console.ReadLine();
+if (x.Length != 5)
+{
+    System.Console.WriteLine("this is not five-digit number");
+}
+else if (x[0] == x[4] && x[1] == x[3])
+{
+    Console.WriteLine("yes, this is palindorum");
+}
+else
+{
+    Console.WriteLine("this is not palindorum");
+}
--- Previous_tasks/olesya/Program.cs
+++ Previous_tasks/olesya/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-richik(((((^^^^^%%%)))))
+﻿
+Console.Write("Print a digit N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 1; i <= n; i++)
+    {
+        int cube = i * i * i;
+        Console.Write($"{cube} ");
+    }
+
+Console.WriteLine();
+    
