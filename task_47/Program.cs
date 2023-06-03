471b5b61f86bc44ec298c32d63f8380a7e993e95
Alexey Agafonov <taganroglad1@gmail.com>
2023-06-03 10:01:22 +0300

    added 47 code

---
+++ task_47/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m = 3; 
+        int n = 4; 
+
+        double[,] array = new double[m, n];
+        Random random = new Random();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = random.NextDouble() * 20 - 10; 
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
