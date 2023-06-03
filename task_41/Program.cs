1ef0f6c6df1524c8a9a50cd2058f1dfcc03ca7bb
Alexey Agafonov <taganroglad1@gmail.com>
2023-06-03 09:45:50 +0300

    added 41 code

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("print numbers, separated by commas: ");
+        string input = Console.ReadLine();
+
+        // Разделение введенной строки на числа
+        string[] numbers = input.Split(',');
+
+        int count = 0;
+
+        // Проверка каждого числа и подсчет положительных чисел
+        foreach (string number in numbers)
+        {
+            int parsedNumber;
+            if (int.TryParse(number.Trim(), out parsedNumber) && parsedNumber > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Numbers of positives are: " + count);
+    }
+}
