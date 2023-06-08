e29b37d0e78d4b423ec229a74de1a464bf0b35c0
Alexey Agafonov <taganroglad1@gmail.com>
2023-06-08 20:47:50 +0300

    just added 66 code

--- tasl_66/Program.cs
+++ tasl_66/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the definition M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the definition N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int sum = CalculateSum(M, N);
+        Console.WriteLine("Sum of numbers from {0} to {1}: {2}", M, N, sum);
+    }
+
+    static int CalculateSum(int start, int end)
+    {
+        if (start > end)
+            return 0;
+
+        return start + CalculateSum(start + 1, end);
+    }
+}
