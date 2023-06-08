ad5bd61b66d07063f4d341fd3bf401109d818355
Alexey Agafonov <taganroglad1@gmail.com>
2023-06-08 20:43:58 +0300

    just added 64 code

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the definition N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        PrintNaturalNumbers(N);
+    }
+
+    static void PrintNaturalNumbers(int number)
+    {
+        if (number > 0)
+        {
+            Console.Write(number + " ");
+            PrintNaturalNumbers(number - 1);
+        }
+    }
+}
