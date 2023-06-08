1681a7563eafba24e64a6b64f832cfd27117f426
Alexey Agafonov <taganroglad1@gmail.com>
2023-06-08 20:51:48 +0300

    just added 68 code

--- task_68/Program.cs
+++ task_68/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the definition m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the definition n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = AckermannFunction(m, n);
+        Console.WriteLine("A({0},{1}) = {2}", m, n, result);
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+
+        if (m > 0 && n == 0)
+            return AckermannFunction(m - 1, 1);
+
+        if (m > 0 && n > 0)
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+
+        return -1; // В случае некорректных входных данных
+    }
+}
