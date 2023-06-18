4e8dda8a8d06547aaa535ca5692c24fc5ab3d536
Alexey Agafonov <taganroglad1@gmail.com>
2023-06-18 12:17:58 +0300

    just added 56 code

--- task_56/Program.cs
+++ task_56/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[,] matrix = {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 },
+            { 5, 2, 6, 7 }
+        };
+
+        int minRowIndex = FindRowWithMinSum(matrix);
+
+        Console.WriteLine("The row with the smallest sum of elements: " + (minRowIndex + 1));
+    }
+
+    static int FindRowWithMinSum(int[,] matrix)
+    {
+        int minSum = int.MaxValue;
+        int minRowIndex = -1;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            int rowSum = 0;
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                rowSum += matrix[i, j];
+            }
+
+            if (rowSum < minSum)
+            {
+                minSum = rowSum;
+                minRowIndex = i;
+            }
+        }
+
+        return minRowIndex;
+    }
+}
