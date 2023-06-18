9f510e8f57cbb1e44160cbfd10b5ae154bec38a9
Alexey Agafonov <taganroglad1@gmail.com>
2023-06-18 12:48:45 +0300

    just added 62 code

--- task_62/Program.cs
+++ task_62/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 4; 
+        int[,] matrix = new int[n, n]; 
+
+        int value = 1; 
+        int minRow = 0;
+        int maxRow = n - 1;
+        int minCol = 0;
+        int maxCol = n - 1;
+
+        while (value <= n * n)
+        {
+            
+            for (int col = minCol; col <= maxCol; col++)
+            {
+                matrix[minRow, col] = value;
+                value++;
+            }
+            minRow++;
+
+            
+            for (int row = minRow; row <= maxRow; row++)
+            {
+                matrix[row, maxCol] = value;
+                value++;
+            }
+            maxCol--;
+
+            
+            for (int col = maxCol; col >= minCol; col--)
+            {
+                matrix[maxRow, col] = value;
+                value++;
+            }
+            maxRow--;
+
+            
+            for (int row = maxRow; row >= minRow; row--)
+            {
+                matrix[row, minCol] = value;
+                value++;
+            }
+            minCol++;
+        }
+
+        
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                Console.Write(matrix[row, col].ToString().PadLeft(2, '0') + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
