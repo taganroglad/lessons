658905107c4868477f561cdec58ce00d9c8ec97c
Alexey agafonov <taganroglad1@gmail.com>
2023-09-09 10:13:50 +0300

    just created ne console

--- butkamp/1st_lesson/Program.cs
+++ butkamp/1st_lesson/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿import pandas as pd
+
+df = pd.read_csv('sample_data/california_housing_train.csv')
+data = pd.read_csv(df)
+
+filtered_data = data[(data['population'] >= 0) & (data['population'] <= 500)]
+average_house_value = filtered_data['median_house_value'].mean()
+print("Средняя стоимость дома с количеством людей от 0 до 500:", average_house_value)
